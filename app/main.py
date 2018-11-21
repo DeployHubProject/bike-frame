74c811f9d172801f1b0255fbff02dd9ae2b683ef
Steve Taylor <steve.taylor@openmakesoftware.com>
2018-11-21 16:13:36 -0700

    Use hostname for db

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ app.config.from_object(__name__)
 CORS(app)
 
 db = None
-dburl = 'mongodb://' + os.getenv('BIKE_DB_SERVICE_HOST','docker.for.mac.localhost') + ':27017'
+dburl = 'mongodb://mongo-0.mongo:27017'
 try:
     client = MongoClient(dburl)
     pprint.pprint(client.server_info())
